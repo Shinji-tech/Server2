7aef0caf5cd9eafe9c133347fc011f25e0bf035c
Shinji404 <20521981@gm.uit.edu.vn>
2023-12-27 22:03:37 +0700

    fix connection

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-require('./Connection')
-
+const userRoutes = require('./routes/userRoutes')
 const app =express();
 const PORT = process.env.PORT || 5000;
 
@@ -10,6 +9,9 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.use('/users', userRoutes)
+require('./Connection')
 
 app.get('/' , (req,res)=> {
     res.json('test ok');
