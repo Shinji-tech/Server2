7f99d8af7cc793f12d5b2ea4cfdd2ce8a8034eb5
Shinji404 <20521981@gm.uit.edu.vn>
2023-12-27 21:45:04 +0700

    Add existing project files prior to the push to GitHub.

---
+++ Connection.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectionString = (`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0.djnpa7q.mongodb.net/?retryWrites=true&w=majority`)
+
+//Connect to MongoDB
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
+  // Get the default connection
+  const db = mongoose.connection;
+  
+  // Event listeners for connection status
+  db.on('error', (error) => {
+    console.error('MongoDB connection error:', error);
+  });
+  
+  db.once('open', () => {
+    console.log('Connected to MongoDB');
+    
+  });
+  
+  db.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+  });
+  
+  // Gracefully close the connection on process termination
+  process.on('SIGINT', () => {
+    db.close(() => {
+      console.log('MongoDB connection closed through app termination');
+      process.exit(0);
+    });
+  });
